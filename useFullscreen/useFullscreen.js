e15f3321f25a1dce5e39dcb5190296db815cb32f
LDG - DGit <hi-ldg@dgit.co>
2022-09-07 02:33:00 +0900

    add useFullscreen

---
+++ useFullscreen/useFullscreen.js
@@ -0,0 +1,36 @@
+export const useFullscreen = (callback) => {
+  const element = useRef();
+  const runCb = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
+  };
+  const triggerFull = () => {
+    if (element.current) {
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullScreen) {
+        element.current.mozRequestFullScreen();
+      } else if (element.current.webkitRequestFullscreen) {
+        document.webkitRequestFullscreen();
+      } else if (document.msRequestFullscreen) {
+        document.msRequestFullscreen();
+      }
+      runCb(true);
+    }
+  };
+  const exitFull = () => {
+    document.exitFullscreen();
+    if (element.exitFullscreen) {
+      element.exitFullscreen();
+    } else if (element.mozCancelFullScreen) {
+      element.mozCancelFullScreen();
+    } else if (element.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    runCb(false);
+  };
+  return { element, triggerFull, exitFull };
+};
